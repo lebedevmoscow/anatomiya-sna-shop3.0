527780f5d021862a9e733550d309ae1174b44ee1
Boris Lebedev <lebedev.moscow.me@gmail.com>
2021-02-27 20:58:13 +0500

    outside click handler

--- components/Popups/ProductPageDesktopPopup.js
+++ components/Popups/ProductPageDesktopPopup.js
@@ -1,5 +1,6 @@
 import styles from './../../styles/components/Popups/ProductPageDesktopPopup.module.sass'
 import { SlideDown } from 'react-slidedown'
+import OutsideClickHandler from 'react-outside-click-handler'
 import { useState, useEffect } from 'react'
 
 const ProductPageDesktopPopup = ({
@@ -17,32 +18,38 @@ const ProductPageDesktopPopup = ({
     }, [popupIsClosed])
 
     return (
-        <div
-            style={{
-                backgroundColor: bgc,
-                border: `1px solid ${borderColor}`,
-                color: txtColor,
-                cursor: dontShowPopup ? 'default' : 'pointer',
+        <OutsideClickHandler
+            onOutsideClick={() => {
+                setPopupIsClosed(true)
             }}
-            className={styles.product_page_desktop_popup}
         >
-            <span onClick={() => setPopupIsClosed((p) => !p)}>{title}</span>
-            <div className={styles.popup__inner__wrap}>
-                <SlideDown
-                    className={styles.popup__inner}
-                    closed={popupIsClosed}
-                >
-                    <div
-                        style={{
-                            padding: '30px 20px 1px',
-                            paddingBottom: '30px',
-                        }}
+            <div
+                style={{
+                    backgroundColor: bgc,
+                    border: `1px solid ${borderColor}`,
+                    color: txtColor,
+                    cursor: dontShowPopup ? 'default' : 'pointer',
+                }}
+                className={styles.product_page_desktop_popup}
+            >
+                <span onClick={() => setPopupIsClosed((p) => !p)}>{title}</span>
+                <div className={styles.popup__inner__wrap}>
+                    <SlideDown
+                        className={styles.popup__inner}
+                        closed={popupIsClosed}
                     >
-                        {text}
-                    </div>
-                </SlideDown>
+                        <div
+                            style={{
+                                padding: '30px 20px 1px',
+                                paddingBottom: '30px',
+                            }}
+                        >
+                            {text}
+                        </div>
+                    </SlideDown>
+                </div>
             </div>
-        </div>
+        </OutsideClickHandler>
     )
 }
 
